dc7c40e12f0a9e6140b51207fd6c4ea0d89b6ac4
oaishllib <secret@billhsiao.com>
2017-11-14 03:42:37 -0800

    third

---
+++ server1.js
@@ -0,0 +1,36 @@
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var index = require('./routes/index');
+//
+//
+// app.get('/', function(req, res){
+//   res.sendFile(__dirname + '/index.html');
+// });
+
+//app.use('/', index);
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});
+
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+//
+// http.listen(3000, function(){
+//   console.log('listening on *:3000');
+// });
