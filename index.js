8f14c984a54d231212b2303a9256b00f1cb6f4bf
oaishllib <secret@billhsiao.com>
2017-11-14 04:29:12 -0800

    example

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var index = require('./routes/index');
-//
-//
-// app.get('/', function(req, res){
-//   res.sendFile(__dirname + '/index.html');
-// });
+var port = process.env.PORT || 3000;
 
-//app.use('/', index);
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
 
 io.on('connection', function(socket){
   console.log('a user connected');
@@ -20,17 +17,16 @@ io.on('connection', function(socket){
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
     console.log('message: ' + msg);
   });
 });
 
-
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+http.listen(port, function(){
+  console.log('listening on *:' + port);
 });
-//
-// http.listen(3000, function(){
-//   console.log('listening on *:3000');
-// });
